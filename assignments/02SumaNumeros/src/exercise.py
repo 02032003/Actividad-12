8d65bb4a3a721455e086409ac1dc92d37ddb89c2
02032003 <89027022+02032003@users.noreply.github.com>
2021-08-30 08:21:06 -0700

    Update exercise.py

--- assignments/02SumaNumeros/src/exercise.py
+++ assignments/02SumaNumeros/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+cont=0
+acum=0
+while acum<=1000:
+    a=int(input())
+    acum+=a
+    cont+=1
+print("suma = ", acum)
+print("cantidad de números = ", cont)
 
 if __name__=='__main__':
     main()
