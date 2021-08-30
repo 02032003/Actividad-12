a582785e96441b10c86df87d502cb629dd2b6ff3
02032003 <89027022+02032003@users.noreply.github.com>
2021-08-30 08:38:09 -0700

    Update exercise.py

--- assignments/03NumeroPrimo/src/exercise.py
+++ assignments/03NumeroPrimo/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+
+a=int(input())
+vcot=1
+sumar=0
+
+while vcot<a:
+    if a%vcot==0:
+        sumar+=vcot
+    vcot+=1
+if a==sumar:
+    print("El número es perfecto")
+else:
+    print("El NO es número perfecto")
 if __name__=='__main__':
     main()
